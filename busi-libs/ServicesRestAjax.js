ccc63910edcc9543e79ce32245769ea7527c3be9
CSNight <chensi__che@163.com>
2018-04-22 19:35:22 +0800

    mmc_client

---
+++ busi-libs/ServicesRestAjax.js
@@ -0,0 +1,109 @@
+/**
+ * 所有的服务Ajax请求
+ */
+function Config() {
+    var URLset = {
+        BaseURI: "http://127.0.0.1:5000/",
+        MapURL: "http://localhost:8090/iserver/services/map-china400/rest/maps/China",
+        FileURI: "http://localhost:8080/ShipAssist-Services/"
+    };
+    this.getUrl = function (key) {
+        return URLset[key];
+    };
+}
+
+var UrlConfig = function () {
+};
+var instance = new Config();
+UrlConfig.getBaseURI = function () {
+    return instance.getUrl('BaseURI');
+};
+UrlConfig.getMapURL = function () {
+    return instance.getUrl('MapURL');
+};
+UrlConfig.getFileURI = function () {
+    return instance.getUrl('FileURI');
+};
+
+/**
+ * 所有的查询服务Ajax请求类
+ */
+function RestQueryAjax(callback) {
+    this.login_REST = function (data) {
+        ResultPost(data, 'user', 'login');
+    };
+    this.check_login_REST = function (data) {
+        ResultGet(data, 'user', 'check_login');
+    };
+    this.logout_REST = function (data) {
+        ResultGet(data, 'user', 'logout');
+    };
+    this.sign_up_REST = function (data) {
+        ResultGet(data, 'user', 'sign_up');
+    };
+    this.delete_user_REST = function (data) {
+        ResultGet(data, 'user', 'delete_user');
+    };
+    // 返回函数
+    RestQueryAjax.prototype = {
+        callback: callback
+    };
+    // 返回值生成器
+    var responseBuilder = function (request, response) {
+        return {
+            request: request,
+            response: response
+        }
+    };
+
+    // POST-Ajax
+    var ResultPost = function postQuery(data, restin, method) {
+        $.ajax({
+            type: 'POST',
+            url: UrlConfig.getBaseURI() + restin + '/' + method,
+            data: data,
+            dataType: 'json',
+            xhrFields: {
+                'Access-Control-Allow-Origin': UrlConfig.getBaseURI(),
+                'Access-Control-Allow-Methods': 'POST',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'withCredentials': true
+            },
+            contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+            success: function (result) {
+                if (result != null) {
+                    callback(responseBuilder(data, result));
+                } else {
+                    callback(responseBuilder(data, '响应错误'));
+                }
+            },
+            error: function (jqXHR, textStatus) {
+                callback(responseBuilder(data, jqXHR.responseText, 0, textStatus));
+            }
+        });
+    };
+
+    // GET-Ajax
+    var ResultGet = function getQuery(data, restin, method) {
+        $.ajax({
+            url: UrlConfig.getBaseURI() + restin + '/' + method,
+            type: "GET",
+            data: data,
+            timeout: 0,
+            contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+            xhrFields: {
+                withCredentials: true
+            },
+            success: function (result) {
+                if (result != null) {
+                    callback(responseBuilder(data, result));
+                } else {
+                    callback(responseBuilder(data, '响应错误'));
+                }
+            },
+            error: function (jqXHR, textStatus) {
+                callback(responseBuilder(data, jqXHR.responseText, 0, textStatus));
+            }
+        });
+    };
+}
--- busi-libs/user_manage/login_module.js
+++ busi-libs/user_manage/login_module.js
@@ -2,11 +2,77 @@ function validateForm() {
     var user = $("#username").val();
     var pwd = $("#pwd").val();
     var code = $("#code").val();
+    var rest_login = new RestQueryAjax(login_callback);
+    var data = {
+        username: user,
+        pwd: pwd,
+        code: code
+    };
+    rest_login.login_REST(data);
 }
 
-$(document).ready(function(){
-
+$(document).ready(function () {
+    getCheckImg();
+    if (getCookie('user') && getCookie('pwd')) {
+        $("#username").val(getCookie('user'));
+        $("#pwd").val(getCookie('pwd'));
+        $('#remember').prop('checked', true);
+    }
+    $('#remember').click(function () {
+        if ($('#remember').prop('checked')) {
+            setCookie('user', $("#username").val(), 1); //保存帐号到cookie，有效期1天
+            setCookie('pwd', $("#pwd").val(), 1); //保存密码到cookie，有效期1天
+        } else {
+            delCookie('user');
+            delCookie('pswd');
+        }
+    });
+    $(document).keydown(function (event) {
+        var e = event || window.event || arguments.callee.caller.arguments[0];
+        if (e && e.keyCode == 13) {
+            $('#submit').click()
+        }
+    });
 });
+
+
+//设置cookie
+function setCookie(name, value, day) {
+    var date = new Date();
+    date.setDate(date.getDate() + day);
+    document.cookie = name + '=' + value + ';expires=' + date;
+}
+
+//获取cookie
+function getCookie(name) {
+    var reg = RegExp(name + '=([^;]+)');
+    var arr = document.cookie.match(reg);
+    if (arr) {
+        return arr[1];
+    } else {
+        return '';
+    }
+}
+
+//删除cookie
+function delCookie(name) {
+    setCookie(name, null, -1);
+}
+
+function login_callback(res) {
+
+}
+
+function getCheckImg() {
+    $("#code_img").attr('src', UrlConfig.getBaseURI() + 'user/verify?w=100&h=36');
+    $("#code_img").click(
+        function () {
+            var time_now = new Date().getTime();
+            $("#code_img").attr(
+                'src',
+                UrlConfig.getBaseURI() + 'user/verify?w=100&h=36&d=' + time_now);
+        });
+}
 
 function customCube(element) {
     var sides = ['top', 'left', 'right'];
